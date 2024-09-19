ec88cc891d92d20d7b1a073f149fd0bcc0139b66
st10032904 <st10032904@vcconnect.edu.za>
2024-09-19 17:40:00 +0200

    Added a home page for user landing

---
+++ Sprint 3 (Prototype)/Prototype (.NET)/Prototype (.NET)/Home.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Prototype__.NET_
+{
+    public partial class Home : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        protected void btnRegister_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Register.aspx");
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("Login.aspx");
+        }
+
+        protected void btnStaffLogin_Click(object sender, EventArgs e)
+        {
+            Response.Redirect("StaffLogin.aspx");
+        }
+    }
+}
